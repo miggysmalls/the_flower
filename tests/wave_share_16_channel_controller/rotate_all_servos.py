e53cdf5b8f6ebafadfc67c8569ffd5f386a9a20e
Miguel Dowgopoluk <mdowgopoluk@westfield.com>
2020-08-07 22:13:55 -0700

    Added 3 loop limit.

--- tests/wave_share_16_channel_controller/rotate_all_servos.py
+++ tests/wave_share_16_channel_controller/rotate_all_servos.py
@@ -2,8 +2,8 @@ import time
 
 
 def test_full_range_of_all_servos(servo_controller_one):
-
-    while True:
+    max_runs = 3
+    while max_runs != 0:
         full_rotate(servo_controller_one, 0)
         full_rotate(servo_controller_one, 1)
         full_rotate(servo_controller_one, 2)
@@ -11,6 +11,7 @@ def test_full_range_of_all_servos(servo_controller_one):
         full_rotate(servo_controller_one, 4)
         full_rotate(servo_controller_one, 5)
         full_rotate(servo_controller_one, 6)
+        max_runs -= 1
 
 
 def full_rotate(servo_controller_one, servo_channel):
