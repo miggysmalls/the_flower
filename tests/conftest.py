32952573008234918f9823fcf9247b59aa73144f
Russian Poundcake <miggysmalls@hotmail.com>
2020-08-07 19:13:50 -0900

    Added all servos to test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,8 @@ import pytest
 from libraries.ServoController import PCA9685
 
 
-@pytest.ficture(scope='module')
-def servo_controller_one(address='0x40', debug=False):
-    controller_one = PCA9685(address, debug=debug)
+@pytest.fixture(scope='module')
+def servo_controller_one():
+    controller_one = PCA9685()
     controller_one.setPWMFreq(50)
     return controller_one
--- tests/rotate_all_servos.py
+++ tests/rotate_all_servos.py
@@ -4,13 +4,18 @@ import time
 def test_full_range_of_all_servos(servo_controller_one):
     while True:
         full_rotate(servo_controller_one, 0)
-
+        full_rotate(servo_controller_one, 1)
+        full_rotate(servo_controller_one, 2)
+        full_rotate(servo_controller_one, 3)
+        full_rotate(servo_controller_one, 4)
+        full_rotate(servo_controller_one, 5)
+        full_rotate(servo_controller_one, 6)
 
 def full_rotate(servo_controller_one, servo_channel):
-    for i in range(500, 2500, 5):
+    for i in range(500, 2500, 100):
         servo_controller_one.setServoPulse(servo_channel, i)
-        time.sleep(0.000001)
+        time.sleep(0.01)
     time.sleep(1)
-    for i in range(2500, 500, -5):
+    for i in range(2500, 500, -100):
         servo_controller_one.setServoPulse(servo_channel, i)
-        time.sleep(0.000001)
+        time.sleep(0.01)
