a9d284cecba9135219c1600b473a676562893e9c
miggysmalls <miggysmalls@users.noreply.github.com>
2020-09-22 08:06:12 -0700

    Update sensor_zones.ino

--- controllers/arduino/sensor_zones.ino
+++ controllers/arduino/sensor_zones.ino
@@ -14,53 +14,53 @@ float distanceCM;
 float distanceInch;
 
 void setup() {
-waken.attach(flowerWake);
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-pinMode(redLED, OUTPUT);
-pinMode(yellowLED, OUTPUT);
-pinMode(greenLED, OUTPUT);
-Serial.begin(9600); 
+  waken.attach(flowerWake);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH);
 
-distanceCM = duration * 0.034 /2;
-distanceInch = duration * 0.0133 /2;
+  distanceCM = duration * 0.034 / 2;
+  distanceInch = duration * 0.0133 / 2;
 
-Serial.print("Distance: ");
-Serial.print(distanceCM);
-Serial.print("  CM  -  ");
+  Serial.print("Distance: ");
+  Serial.print(distanceCM);
+  Serial.print("  CM  -  ");
 
-Serial.print(distanceInch);
-Serial.println("  Inches");
+  Serial.print(distanceInch);
+  Serial.println("  Inches");
 
-if (distanceCM >= 100) {
-  waken.write(0);
-  digitalWrite(greenLED, HIGH);
-  digitalWrite(yellowLED, LOW);
-  digitalWrite(redLED, LOW);
-}
-else if (distanceCM < 100 &&  distanceCM > 50) {  
-  waken.write(90);
-  digitalWrite(greenLED, LOW);
-  digitalWrite(yellowLED, HIGH);
-  digitalWrite(redLED, LOW);
-}
-else {
-  waken.write(180);
-  digitalWrite(greenLED, LOW);
-  digitalWrite(yellowLED, LOW);
-  digitalWrite(redLED, HIGH);  
-}
+  if (distanceCM >= 100) {
+    waken.write(0);
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, LOW);
+  }
+  else if (distanceCM < 100 &&  distanceCM > 50) {
+    waken.write(90);
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, HIGH);
+    digitalWrite(redLED, LOW);
+  }
+  else {
+    waken.write(180);
+    digitalWrite(greenLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, HIGH);
+  }
 
-delay(1000);
+  delay(1000);
 }
